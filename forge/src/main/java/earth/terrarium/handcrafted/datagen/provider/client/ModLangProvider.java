0fd66e8e243a40236de484b671bd0e9129511309
Alex Nijjar <alexander.nijjar@icloud.com>
2023-10-16 10:09:51 -0500

    Add Painting translations (#60)

--- forge/src/main/java/earth/terrarium/handcrafted/datagen/provider/client/ModLangProvider.java
+++ forge/src/main/java/earth/terrarium/handcrafted/datagen/provider/client/ModLangProvider.java
@@ -84,5 +84,62 @@ public class ModLangProvider extends LanguageProvider {
         add("tag.item.handcrafted.plates", "Plates");
         add("tag.item.handcrafted.bowls", "Bowls");
         add("tag.item.handcrafted.crockery_combos", "Crockery Combos");
+
+        add("painting.handcrafted.pride_steve.author", "Kekie6");
+        add("painting.handcrafted.pride_steve.title", "Pride Steve");
+        add("painting.handcrafted.beach_sunrise_mid.author", "Kekie6");
+        add("painting.handcrafted.beach_sunrise_mid.title", "Beach Sunrise");
+        add("painting.handcrafted.apple.author", "Kekie6");
+        add("painting.handcrafted.apple.title", "Apple");
+        add("painting.handcrafted.small_safari_sunset.author", "Kekie6");
+        add("painting.handcrafted.small_safari_sunset.title", "Safari Sunset");
+        add("painting.handcrafted.marigold_meadows.author", "Kekie6");
+        add("painting.handcrafted.marigold_meadows.title", "Marigold Meadows");
+        add("painting.handcrafted.ad_astra.author", "Kekie6");
+        add("painting.handcrafted.ad_astra.title", "Ad Astra");
+
+        add("painting.handcrafted.misty_mountains.author", "Kekie6");
+        add("painting.handcrafted.misty_mountains.title", "Misty Mountains");
+        add("painting.handcrafted.coral_depths.author", "Kekie6");
+        add("painting.handcrafted.coral_depths.title", "Coral Depths");
+        add("painting.handcrafted.broken_birches.author", "Kekie6");
+        add("painting.handcrafted.broken_birches.title", "Broken Birches");
+        add("painting.handcrafted.mysterious_mangroves.author", "Kekie6");
+        add("painting.handcrafted.mysterious_mangroves.title", "Mysterious Mangroves");
+        add("painting.handcrafted.my_man_diorite.author", "Kekie6");
+        add("painting.handcrafted.my_man_diorite", "My Man Diorite");
+        add("painting.handcrafted.cookies_on_a_plate.author", "Kekie6");
+        add("painting.handcrafted.cookies_on_a_plate.title", "Cookies on a Plate");
+        add("painting.handcrafted.lava_puddles.author", "Kekie6");
+        add("painting.handcrafted.lava_puddles.title", "Lava Puddles");
+        add("painting.handcrafted.the_cat_and_the_cup.author", "Kekie6");
+        add("painting.handcrafted.the_cat_and_the_cup.title", "The Cat and the Cup");
+        add("painting.handcrafted.terrarium.author", "Kekie6");
+        add("painting.handcrafted.terrarium.title", "Terrarium");
+
+        add("painting.handcrafted.green_woods.author", "Kekie6");
+        add("painting.handcrafted.green_woods.title", "Green Woods");
+        add("painting.handcrafted.safari_sunset.author", "Kekie6");
+        add("painting.handcrafted.safari_sunset.title", "Safari Sunset");
+        add("painting.handcrafted.rocky_beach.author", "Kekie6");
+        add("painting.handcrafted.rocky_beach.title", "Rocky Beach");
+
+        add("painting.handcrafted.misty_mountain_left_sidepanel.author", "Kekie6");
+        add("painting.handcrafted.misty_mountain_left_sidepanel.title", "Misty Mountain Left Sidepanel");
+        add("painting.handcrafted.misty_mountain_right_sidepanel.author", "Kekie6");
+        add("painting.handcrafted.misty_mountain_right_sidepanel.title", "Misty Mountain Right Sidepanel");
+        add("painting.handcrafted.broken_birches_left_sidepanel.author", "Kekie6");
+        add("painting.handcrafted.broken_birches_left_sidepanel.title", "Broken Birches Left Sidepanel");
+        add("painting.handcrafted.broken_birches_right_sidepanel.author", "Kekie6");
+        add("painting.handcrafted.broken_birches_right_sidepanel.title", "Broken Birches Right Sidepanel");
+
+        add("painting.handcrafted.desert_plateau.author", "Kekie6");
+        add("painting.handcrafted.desert_plateau.title", "Desert Plateau");
+        add("painting.handcrafted.beach_sunrise_right.author", "Kekie6");
+        add("painting.handcrafted.beach_sunrise_right.title", "Beach Sunrise Right");
+        add("painting.handcrafted.beach_sunrise_left.author", "Kekie6");
+        add("painting.handcrafted.beach_sunrise_left.title", "Beach Sunrise Left");
+        add("painting.handcrafted.nian_cat.author", "Kekie6");
+        add("painting.handcrafted.nian_cat.title", "Nian Cat");
     }
 }
